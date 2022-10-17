eb922443f9166fdf92b4dae1bafc8697c39b4fd6
KivruJ <kivru2010@yandex.ru>
2022-10-17 16:43:50 +0300

    Init2

--- Program.cs
+++ Program.cs
@@ -6,35 +6,35 @@
 
 // M = 4; N = 8. -> ""4, 6, 7, 8""
 
-// Console.WriteLine("Введите число M");
-// int M = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите число N");
-// int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
 
-// string PrintNumbers (int start, int end)
-// {
-//     if(start==end) return start.ToString();
-//     return(start + "," + PrintNumbers(start+1, end) );
-// }
-// Console.WriteLine($"Числа от {M} до {N}: {PrintNumbers(M, N)}");
+string PrintNumbers (int start, int end)
+{
+    if(start==end) return start.ToString();
+    return(start + "," + PrintNumbers(start+1, end) );
+}
+Console.WriteLine($"Числа от {M} до {N}: {PrintNumbers(M, N)}");
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-// Console.WriteLine("Введите число M");
-// int M = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите число N");
-// int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
 
-// int SumNumbers (int start, int end)
-// {
-//     if (start > end) return Console.WriteLine("Введите другие числа");
-//     if (start == end) return start;
-//    return(start + SumNumbers(start+1, end));
-// }
-// Console.WriteLine($"Числа  {M} и {N}, их сумма: {SumNumbers(M, N)}");
+int SumNumbers (int start, int end)
+{
+    if (start > end) return Console.WriteLine("Введите другие числа");
+    if (start == end) return start;
+   return(start + SumNumbers(start+1, end));
+}
+Console.WriteLine($"Числа  {M} и {N}, их сумма: {SumNumbers(M, N)}");
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
